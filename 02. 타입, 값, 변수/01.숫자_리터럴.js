25eac7f74ce0392ea7be967b60b7733a82565396
t2 <oo_ri@naver.com>
2022-04-10 06:37:16 +0900

    숫자 리터럴 업데이트

--- 02. 타입, 값, 변수/01.숫자_리터럴.js
+++ 02. 타입, 값, 변수/01.숫자_리터럴.js
@@ -8,11 +8,20 @@ console.log();
 
 console.log(0xff);
 console.log(0xbadcafe);
-
-console.log();
-
 console.log(0b10101);
+console.log(0o377);
 
 console.log();
 
-console.log(0o377);
+console.log(3.14);
+console.log(2345.6789);
+console.log(0.333333333333333333);
+console.log(6.02e23);
+console.log(1.4738223e-32);
+
+console.log();
+
+console.log(1_000_000_000);
+console.log(0x89_ab_cd_ef);
+console.log(0b0001_1101_0111);
+console.log(0.123_456_789);
