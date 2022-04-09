f6fb729acdde3dac0b0e1956022375a953cee7e0
t2 <oo_ri@naver.com>
2022-04-10 06:30:58 +0900

    숫자 리터럴 업데이트

---
+++ 02. 타입, 값, 변수/01.숫자_리터럴.js
@@ -0,0 +1,18 @@
+// 숫자 리터럴: 자바스크립트 프로그램에 직접 기입한 숫자
+
+console.log(0);
+console.log(3);
+console.log(10000000);
+
+console.log();
+
+console.log(0xff);
+console.log(0xbadcafe);
+
+console.log();
+
+console.log(0b10101);
+
+console.log();
+
+console.log(0o377);
