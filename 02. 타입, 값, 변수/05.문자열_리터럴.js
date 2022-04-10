0fdbdc3de2c98b535c84ae1768ca72ed4572f889
t2 <oo_ri@naver.com>
2022-04-10 17:24:26 +0900

    문자열 리터럴 업데이트

---
+++ 02. 타입, 값, 변수/05.문자열_리터럴.js
@@ -0,0 +1,14 @@
+console.log(''); // 빈 문자열
+console.log('JavaScript');
+console.log('3.14');
+console.log('"JavaScript"');
+console.log("JavaScript's");
+console.log(`"She said 'hi'"`);
+
+console.log();
+
+console.log('two\nlines'); // 두 행을 한 행으로 표현
+console.log('two\
+lines'); // 한 행을 두 행으로 작성
+console.log(`two
+lines`); // 두 행을 두 행으로 표현
