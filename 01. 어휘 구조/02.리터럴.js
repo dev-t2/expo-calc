d822ed41809cdd9c98b5fd5cc45c22d1873cc289
dev-t2 <oo_ri@naver.com>
2022-10-24 10:58:40 +0900

    Pedometer POC Repository Reset

--- 01. 어휘 구조/02.리터럴.js
+++
@@ -1,8 +0,0 @@
-// 리터럴(Literal): 프로그램 안에 직접 쓴 데이터 값
-
-console.log(12);
-console.log(1.2);
-console.log('Hello JavaScript');
-console.log(true);
-console.log(false);
-console.log(null);
--- 02. 타입, 값, 변수/04.날짜와_시간.js
+++
@@ -1,9 +0,0 @@
-console.log(Date.now()); // 현재 시간을 타임스탬프(숫자)로 표현
-
-console.log();
-
-const now = new Date();
-
-console.log(now); // 현재 시간을 Date 객체로 표현
-console.log(now.getTime()); // 타임스탬프(숫자)로 변환
-console.log(now.toISOString()); // 표준 형식의 문자열로 변환
